ae79edfd37da9f0f3bdbe7fde098fa2e2d92bd49
Pierre Klink <dev@klinks.info>
2012-10-18 20:55:07 +0200

    prepare for 0.3.0

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $yii=dirname(__FILE__).'/yii/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
