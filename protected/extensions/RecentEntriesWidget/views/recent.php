1a04b3fd055ed3842b8cf034ee95911c8dee1cbb
Pierre Klink <dev@klinks.info>
2012-10-30 21:57:12 +0100

    Bug: Password isn't shown by viewing entry from sidebar-widget

--- protected/extensions/RecentEntriesWidget/views/recent.php
+++ protected/extensions/RecentEntriesWidget/views/recent.php
@@ -12,7 +12,7 @@
                 <li>
                     <?php echo CHtml::link(CHtml::encode($model->name), array('/entry/update', 'id' => $model->id), array(
                         'data-reveal-id' => 'entry-form-modal',
-                        'rel'            => CHtml::normalizeUrl(array('/entry/getData', 'id' => $model->id)),
+                        'rel'            => CHtml::normalizeUrl(array('/entry/getData', 'id' => $model->id, 'withPassword' => 1)),
                         'class'          => 'update-entry',
                     )) ?>
                 </li>
