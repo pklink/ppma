19a402a43de940af9d33f38640f91d2fe507efaa
Pierre Klink <dev@klinks.info>
2012-10-30 22:05:18 +0100

    added default name for the recent-entry-widget entries

--- protected/extensions/RecentEntriesWidget/views/recent.php
+++ protected/extensions/RecentEntriesWidget/views/recent.php
@@ -10,7 +10,7 @@
         <ul>
             <?php foreach ($models as $model) : ?>
                 <li>
-                    <?php echo CHtml::link(CHtml::encode($model->name), array('/entry/update', 'id' => $model->id), array(
+                    <?php echo CHtml::link((strlen($model->name) == 0 ? '<i>- no name given -</i>' : CHtml::encode($model->name)), array('/entry/update', 'id' => $model->id), array(
                         'data-reveal-id' => 'entry-form-modal',
                         'rel'            => CHtml::normalizeUrl(array('/entry/getData', 'id' => $model->id, 'withPassword' => 1)),
                         'class'          => 'update-entry',
