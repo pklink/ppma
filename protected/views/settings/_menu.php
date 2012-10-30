a5001069cc3ca3a2369bc931ba5115aeef99981d
Pierre Klink <dev@klinks.info>
2012-10-30 22:09:21 +0100

    removed unused file

--- protected/views/settings/_menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $this->menu = array(
-        array('label' => 'Application',     'url' => array('application'), 'visible' => Yii::app()->user->isAdmin),
-        array('label' => 'Change Password', 'url' => array('password')),
-    );
-    
-?>
