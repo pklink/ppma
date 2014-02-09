0b0bec7d32aea5149ce1d96e3dccf7112136b9ca
Pierre Klink <dev@klinks.info>
2014-02-09 18:25:49 +0100

    fixed Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function (grunt) {
                 'release/protected/commands/',
                 'release/protected/migrations/',
                 'release/protected/runtime/*',
-                'release/protected/tests/',
+                'release/protected/tests/'
             ]
         },
         compress: {
