d00c14cd72ea998b3190372b32b2377b5e9143c9
Pierre Klink <dev@klinks.info>
2014-02-09 16:50:38 +0100

    removed unused node modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,6 @@ module.exports = function(grunt) {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            },
             ppma: {
                 files: {
                     'js/ppma.min.js': [
