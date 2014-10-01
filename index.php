01b2c4bb9646494f4793e3dbfbc7e61ff3dfcfd4
Pierre Klink <dev@klinks.info>
2014-10-01 21:24:12 +0200

    updated version number

--- index.php
+++ index.php
@@ -9,5 +9,6 @@ $config = dirname(__FILE__) . '/protected/config/main.php';
 //defined('YII_DEBUG') or define('YII_DEBUG',true);
 //defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+/** @noinspection PhpIncludeInspection */
 require_once($yii);
 Yii::createWebApplication($config)->run();
--- protected/config/ppma.php
+++ protected/config/ppma.php
@@ -9,5 +9,5 @@ return array(
             'name' => 'ppma',
         ),
     'isInstalled' => false,
-    'version' => '0.4.2',
+    'version' => '0.5.0',
 );
