c6fc61665fe8a09ebc45b298b4d68207b8b951dd
Pierre Klink <dev@klinks.info>
2015-02-06 14:51:57 +0100

    Fixed #44: templates for deleting a tag or entry was not compatible with PHP < 5.4

--- protected/views/entry/delete.php
+++ protected/views/entry/delete.php
@@ -2,15 +2,15 @@
     /* @var Entry $model */
 ?>
 
-<h1>Delete Entry '<i><?= $model->name ?></i>'</h1>
+<h1>Delete Entry '<i><?php echo $model->name ?></i>'</h1>
 
-<?= CHtml::beginForm() ?>
+<?php echo CHtml::beginForm() ?>
 
     <p>Do you really want to delete this entry?</p>
 
-    <a href="<?= CHtml::normalizeUrl(array('entry/index')) ?>">
+    <a href="<?php echo CHtml::normalizeUrl(array('entry/index')) ?>">
         <?php echo CHtml::button('Cancel', array('class' => 'secondary button radius'))?>
     </a>
     <?php echo CHtml::submitButton('Yep, delete this entry', array('class' => 'alert button radius'))?>
 
-<?= CHtml::endForm();
+<?php echo CHtml::endForm();
--- protected/views/tag/delete.php
+++ protected/views/tag/delete.php
@@ -2,15 +2,15 @@
     /* @var Tag $model */
 ?>
 
-<h1>Delete Tag '<i><?= $model->name ?></i>'</h1>
+<h1>Delete Tag '<i><?php echo $model->name ?></i>'</h1>
 
-<?= CHtml::beginForm() ?>
+<?php echo CHtml::beginForm() ?>
 
     <p>Do you really want to delete this tag?</p>
 
-    <a href="<?= CHtml::normalizeUrl(array('tag/index')) ?>">
+    <a href="<?php echo CHtml::normalizeUrl(array('tag/index')) ?>">
         <?php echo CHtml::button('Cancel', array('class' => 'secondary button radius'))?>
     </a>
     <?php echo CHtml::submitButton('Yep, delete this tag', array('class' => 'alert button radius'))?>
 
-<?= CHtml::endForm();
+<?php echo CHtml::endForm();
