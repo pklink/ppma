9d93585b7866e324885752773a1e7a28dd3b3f04
Pierre Klink <dev@einself.net>
2015-10-07 17:45:34 +0200

    added missing migration file

---
+++ database/migrations/2015_09_25_144724_create_category_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+
+
+}
