6434787dde3a2d79c53c9f90524324fea6974193
Pierre Klink <dev@klinks.info>
2016-01-20 07:53:43 +0100

    #58 Typo

--- protected/views/entry/_form.php
+++ protected/views/entry/_form.php
@@ -40,7 +40,7 @@
     <?php echo $form->error($model, 'url'); ?>
 
     <?php echo $form->labelEx($model, 'tagList'); ?>
-    <?php echo $form->textField($model, 'tagList', array('placeholder' => 'seperate by commas')); ?>
+    <?php echo $form->textField($model, 'tagList', array('placeholder' => 'separate by commas')); ?>
     <?php echo $form->error($model, 'tagList'); ?>
 
     <?php echo $form->labelEx($model, 'comment'); ?>
