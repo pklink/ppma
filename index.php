149ae902e252a108e6d63abe9a597bf2c9e26966
Pierre Klink <dev@klinks.info>
2016-03-09 17:39:13 +0100

    #60 Moved DEBUG constants to the top of the file

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
+// remove the following lines when in production mode
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
 // change the following paths if necessary
 $yii    = dirname(__FILE__) . '/vendor/yiisoft/yii/framework/yii.php';
 $config = dirname(__FILE__) . '/protected/config/main.php';
 // $config=dirname(__FILE__).'/protected/config/test.php'; // for testing
-
-// remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 /** @noinspection PhpIncludeInspection */
 require_once($yii);
