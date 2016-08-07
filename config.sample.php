3dde3e1fc57682413a8b13339a68f8e13ae55b31
Pierre Klink <dev@klink.xyz>
2016-08-08 01:00:37 +0200

    added sql-dump for tests

--- config.sample.php
+++ config.sample.php
@@ -2,6 +2,16 @@
 
 return [
     'settings' => [
-        'displayErrorDetails' => true
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver'    => 'mysql',
+            'host'      => '127.0.0.1',
+            'database'  => 'ppma',
+            'username'  => 'root',
+            'password'  => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
     ],
 ];
