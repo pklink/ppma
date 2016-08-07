0f78ffc5c96b7c8392b521a304919b24d0fef77a
Pierre Klink <dev@klink.xyz>
2016-08-08 01:03:57 +0200

    added sql-dump for tests

--- config.sample.php
+++ config.sample.php
@@ -8,7 +8,7 @@ return [
             'host'      => '127.0.0.1',
             'database'  => 'ppma',
             'username'  => 'root',
-            'password'  => 'password',
+            'password'  => '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
